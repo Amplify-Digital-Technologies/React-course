4009f4d665bd7e2616e75072ff339775675d3153
Amplify-Digital-Technologies <amplifydt.info@gmail.com>
2025-06-01 20:08:05 +0100

    Intro to useState

--- src/App.js
+++ src/App.js
@@ -1,15 +1,21 @@
 import './App.css';
 import Employee from './components/Employee';
+import { useState } from 'react';
 
 function App() {
+  const [role, setRole] = useState('Dev');
   const showEmployees = true;  
   return (
-    <div className="App">
+    <div className='App'>
       {showEmployees ? (
         <>
-          <Employee name="Shedrack" role="Intern"/>
-          <Employee name="David"/>
-          <Employee Name="Chidi"/>
+          <input type='text' onChange={(e) => {
+            console.log(e.target.value);
+            setRole(e.target.value);
+          }} />
+          <Employee name='Shedrack' role='Intern'/>
+          <Employee name='David' role={role}/>
+          <Employee Name='Chidi'/>
         </>
       ) : (
         <p>You don't have access to employees</p>
